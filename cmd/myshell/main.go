5795e7a970fffef8fb7ded078295d47b5c75e4dc
nutella399 <nutella399@gmail.com>
2024-12-16 16:56:58 -0800

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -13,7 +13,18 @@ func main() {
 	for {
 		repl(reader)
 	}
+}
 
+func isBuiltin(command string) bool {
+	switch command {
+	case
+		"echo",
+		"exit",
+		"type":
+		return true
+	default:
+		return false
+	}
 }
 
 func repl(reader *bufio.Reader) {
@@ -23,11 +34,20 @@ func repl(reader *bufio.Reader) {
 	text = text[:len(text)-1]
 	textArr := strings.Split(text, " ")
 
-	switch textArr[0] {
+	command := textArr[0]
+	args := textArr[1:]
+
+	switch command {
 	case "exit":
 		os.Exit(0)
 	case "echo":
-		fmt.Println(strings.Join(textArr[1:], " "))
+		fmt.Println(strings.Join(args, " "))
+	case "type":
+		if isBuiltin(args[0]) {
+			fmt.Println(args[0] + " is a shell builtin")
+		} else {
+			fmt.Println(args[0] + ": not found")
+		}
 	default:
 		fmt.Println(text + ": command not found")
 	}
